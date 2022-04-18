a4a2709c7d354dbe51a355a4bb019955781d59f6
zakoshgo <81923383+zakoshgo@users.noreply.github.com>
2022-04-18 13:37:36 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -11,19 +11,3 @@ public class Main {
 
 
 }
- class watermelonn {
-
-
-    private int w;
-
-    public watermelonn(int w) {
-        this.w = w;
-    }
-    public String checkDevide(){
-        if(w>100 || w<0) return "ERROR";
-        else if(w%2 ==0&& w > 2) return "YES";
-        else
-            return "NO";
-
-    }
-}
